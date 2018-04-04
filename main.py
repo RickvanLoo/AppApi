ad742f503ad2a7adec0ef0cb0bc4d69b5d6dc9f8
Rivalo <rivalo@home.nl>
2018-04-04 14:46:33 +0200

    Automatic bluetooth fixed, working on info

--- main.py
+++ main.py
@@ -102,7 +102,7 @@ def sendVolume(spi):
 
 def getBluetooth(props):
     Dict = props.GetAll("org.bluez.MediaPlayer1")
-    BTData = json.dumps(Dict)
+    global BTData = json.dumps(Dict)
     #GlobalPlayerInfo['title'] = Jsonprops['Track']['Title']
     print BTData
     print type(BTData)
