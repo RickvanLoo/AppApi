43fafdbd724e0ba15d1b9acf1fadac4fb15c0667
Rivalo <rivalo@home.nl>
2018-04-06 11:51:11 +0200

    Color test

--- main.py
+++ main.py
@@ -4,8 +4,19 @@ import os
 import spidev
 import dbus
 import commands
+from neopixel import *
+
 
 from waitress import serve
+
+# LED strip configuration:
+LED_COUNT      = 3      # Number of LED pixels.
+LED_PIN        = 18      # GPIO pin connected to the pixels (18 uses PWM!).
+LED_FREQ_HZ    = 800000  # LED signal frequency in hertz (usually 800khz)
+LED_DMA        = 10      # DMA channel to use for generating signal (try 10)
+LED_BRIGHTNESS = 255     # Set to 0 for darkest and 255 for brightest
+LED_INVERT     = False   # True to invert the signal (when using NPN transistor level shift)
+LED_CHANNEL    = 0       # set to '1' for GPIOs 13, 19, 41, 45 or 53
 
 config = json.load(open('./serverConfig.json'))
 CurrDirStr = str(os.getcwd());
@@ -133,13 +144,26 @@ def UpdateDataHandler():
     GlobalPlayerInfo['status'] = BTData['Status']
 
 
-
+def SetSTRIPColor(strip, ledinfo):
+    R = ledinfo['r']
+    G = ledinfo['g']
+    B = ledinfo['b']
+    color = Color(R,G,B)
+    for i in range(strip.numPixels()):
+        strip.setPixelColor(i, color)
+        strip.show()
 
 #SPI
 spi = spidev.SpiDev()
 spi.open(0, 0)
 spi.max_speed_hz = 1000000
 sendVolume(spi)
+
+
+#LED SHIT
+strip = Adafruit_NeoPixel(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS, LED_CHANNEL)
+strip.being()
+SetSTRIPColor(strip, GlobalLedInfo)
 
 
 #Define DBUS
